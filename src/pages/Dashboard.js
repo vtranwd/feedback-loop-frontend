437d53f7f02c4db3e44836d0d46ae171def9b83d
vtranwd <vtranwd@gmail.com>
2026-09-17 15:48:48 -0700

    dashboard error fixed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -72,7 +72,6 @@ export default function Dashboard() {
           }`,
         }),
       });
-      const data = await response.json();
       setFeedbackText('');
       fetchFeedback();
     } catch (error) {
